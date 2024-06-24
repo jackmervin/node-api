73fcc62a20e8884bf1081b90481ac3019f50b68d
jackmervin <mervinofficial@gmail.com>
2024-06-24 21:47:37 +0530

    Creating CURD Functionalty

--- main.js
+++ main.js
@@ -7,6 +7,20 @@ app.get("/", (req, res) => {
   res.json({ msg: "hello World" });
 });
 
+//CURD Functionality
+
+//R - for Reading
+app.get("/movies", () => {});
+
+//C - for Create
+app.post("/movies", () => {});
+
+//U - for Update
+app.put("/movies", () => {});
+
+//D - for Delete
+app.put("/movies", () => {});
+
 app.listen(PORT, () => {
   console.log(`The server running at http://localhost:${PORT}`);
 });
