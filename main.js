41874b4249a943c75c9456863ab681659d0010d5
jackmervin <mervinofficial@gmail.com>
2024-06-24 23:06:42 +0530

    MongoDB Conneted

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import express from "express";
 import movieRouter from "./routes/movies.routes.js";
+import connectDB from "./lib/db.js";
 const app = express();
 const PORT = 6969;
+
+//Connect DB
+connectDB();
 
 app.get("/", (req, res) => {
   res.json({ msg: "hello World" });
